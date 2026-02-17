998022b89686c325c47aa66bff6c1942cf66c6dc
Philbenzy <467267056@qq.com>
2026-02-17 20:12:02 +0800

    忽略中间文件

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,10 @@
 import cot_reports as cot
 
 # 获取Disaggregated报告（包含Managed Money分类）
-df = cot.cot_year(year=2026, cot_report_type='legacy_fut')
+df = cot.cot_year(year=2026, cot_report_type='traders_in_financial_futures_futopt')
 
 # 筛选黄金数据
 # 如何打印
 
 # 指定sheet名称
-df.to_excel('gold_cot_data.xlsx', sheet_name='黄金COT数据', index=False)
+df.to_excel('ttf.xlsx', sheet_name='TTF', index=False)
